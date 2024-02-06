cdf137952cf8164ac31849f25d3d1fb5434fd6f7
4lph4 <b4rryd4le@gmail.com>
2024-02-06 10:11:50 +0000

    Update and Delete

--- Contact/views.py
+++ Contact/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ContactForm
 from .models import Contact
 
@@ -6,11 +6,42 @@ from .models import Contact
 
 
 def index(request):
-    contact=Contact.objects.all()
-    if request.method=='POST':
-        form=ContactForm(request.POST)
-        if form.is_valid():
-            form.save()
-        return redirect('/')
+    contact = Contact.objects.all()
+    if request.method == "POST":
+        if "edit" in request.POST:
+            pk = request.POST.get("edit")
+
+            contact_obj = Contact.objects.get(pk=pk)
+            editForm = ContactForm(instance=contact_obj)
+            return render(
+                request,
+                "index.html",
+                {"editForm": editForm, "contacts": contact, "contact_id": pk},
+            )
+        elif "edited" in request.POST:
+            pk=request.POST.get("edited")
+            contact_obj = Contact.objects.get(pk=pk)
+            editedForm = ContactForm(request.POST, instance=contact_obj)
+            if editedForm.is_valid:
+                editedForm.save()
+            return redirect("/")
+
+        else:
+            AddForm = ContactForm(request.POST)
+            if AddForm.is_valid():
+                AddForm.save()
+
+            return redirect("/")
     else:
-        return render(request, "index.html",{"form":ContactForm,'contacts':contact})
+
+        addForm = ContactForm()
+        return render(request, "index.html", {"addForm": addForm, "contacts": contact})
+
+
+
+
+
+def delete(request, pk):
+    contact_obj = get_object_or_404(Contact, id=pk)
+    contact_obj.delete()
+    return redirect("/")
