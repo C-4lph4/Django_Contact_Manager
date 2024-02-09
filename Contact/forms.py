bcde2144f780da2c16ca95396c6e045fab05d9cb
4lph4 <b4rryd4le@gmail.com>
2024-02-09 09:41:17 +0000

    Responsive design

--- Contact/forms.py
+++ Contact/forms.py
@@ -12,3 +12,15 @@ class ContactForm(forms.ModelForm):
             "phone_number": forms.TextInput(attrs={"class": "form_style"}),
             "address": forms.TextInput(attrs={"class": "form_style"}),
         }
+        labels = {
+            "name": "Name",
+            "email": "Email",
+            "phone_number": "Phone Number",
+            "address": "Address",
+        }
+        labels_attrs = {
+            "name": {"class": "label_style"},  
+            "email": {"class": "label_style"},
+            "phone_number": {"class": "label_style"},
+            "address": {"class": "label_style"},
+        }
